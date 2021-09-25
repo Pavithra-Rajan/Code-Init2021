4f6a20b118cb3e460307cf1ae1bde52243e05156
lenoahchacko@gmail.com <lenoahchacko@gmail.com>
2021-09-26 02:40:45 +0530

    Table practical logic+css fixed

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,49 +2,26 @@ import React, { useState } from 'react'
 import { useEffect } from 'react'
 
 export default function Table({final}) {
-    let slotlist=[ 
-        {
-            "day":"Monday",
-            "slots":["A","F","D","B","G","E","-","-","H"]
-        },
-        {
-            "day":"Tuesday",
-            "slots":["B","G","E","C","A","F","-","-","H"]
-        },
-        {
-            "day":"Wednesday",
-            "slots":["C","A","F","D","H","G","-","-","E"]
-        },
-        {
-            "day":"Thursday",
-            "slots":["D","B","G","E","-","C","-","-","G"]
-        },
-        {
-            "day":"Friday",
-            "slots":["E","C","A","F","H","B","-","-","D"]
-        }
+    let tslotlist=[ 
+        {"day":"Monday","slots":["A","F","D","B","G","E","-","-","H"]},
+        {"day":"Tuesday","slots":["B","G","E","C","A","F","-","-","H"]},
+        {"day":"Wednesday","slots":["C","A","F","D","H","G","-","-","E"]},
+        {"day":"Thursday","slots":["D","B","G","E","-","C","-","-","G"]},
+        {"day":"Friday","slots":["E","C","A","F","H","B","-","-","D"]}
+    ]
+    let pslotlist=[ 
+        {"day":"Monday","slots":"P"},
+        {"day":"Tuesday","slots":"Q"},
+        {"day":"Wednesday","slots":"R"},
+        {"day":"Thursday","slots":"S"},
+        {"day":"Friday","slots":"T"}
     ]
     const [model2, setModel2] = useState([
-        {
-            "day":"Monday",
-            "slots":[]
-        },
-        {
-            "day":"Tuesday",
-            "slots":[]
-        },
-        {
-            "day":"Wednesday",
-            "slots":[]
-        },
-        {
-            "day":"Thursday",
-            "slots":[]
-        },
-        {
-            "day":"Friday",
-            "slots":[]
-        }
+        {"day":"Monday","slots":[]},
+        {"day":"Tuesday","slots":[]},
+        {"day":"Wednesday","slots":[]},
+        {"day":"Thursday","slots":[]},
+        {"day":"Friday","slots":[]}
     ]);
     useEffect(()=>{
         console.log("buha",final)
@@ -54,42 +31,48 @@ export default function Table({final}) {
     function finaltomodel2()
     {
         var model2temp=[
-            {
-                "day":"Monday",
-                "slots":[]
-            },
-            {
-                "day":"Tuesday",
-                "slots":[]
-            },
-            {
-                "day":"Wednesday",
-                "slots":[]
-            },
-            {
-                "day":"Thursday",
-                "slots":[]
-            },
-            {
-                "day":"Friday",
-                "slots":[]
-            }]
+            {"day":"Monday","slots":[]},
+            {"day":"Tuesday","slots":[]},
+            {"day":"Wednesday","slots":[]},
+            {"day":"Thursday","slots":[]},
+            {"day":"Friday","slots":[]}
+        ]
         for(var j=0;j<5;j++)
         {
             for(var i=0;i<9;i++)
             {
-                if(slotlist[j]["slots"][i]=='-')
+                if(tslotlist[j]["slots"][i]=='-')
                 {
                     model2temp[j]["slots"].push("-")
                 }
-                else if(final[slotlist[j]["slots"][i]]=="")
+                else if(final[tslotlist[j]["slots"][i]]=="")
                 {
                     model2temp[j]["slots"].push("-")
                 }
                 else
                 {
-                    model2temp[j]["slots"].push(final[slotlist[j]["slots"][i]])
+                    model2temp[j]["slots"].push(final[tslotlist[j]["slots"][i]])
                 }
+            }
+        }
+        var tr=document.getElementsByTagName("tr")
+        console.log("tr",tr)
+        for(var j=0;j<5;j++)
+        {
+            if(final[pslotlist[j]["slots"]]!="")
+            {
+                tr[j+1].children[6].classList.add("left")
+                tr[j+1].children[7].classList.add("cen")
+                tr[j+1].children[8].classList.add("right")
+                model2temp[j].slots[5]=""
+                model2temp[j].slots[6]=final[pslotlist[j]["slots"]]
+                model2temp[j].slots[7]=""
+            }
+            else
+            {
+                tr[j+1].children[6].classList.remove("left")
+                tr[j+1].children[7].classList.remove("cen")
+                tr[j+1].children[8].classList.remove("right")
             }
         }
         console.log("temp",model2temp)
