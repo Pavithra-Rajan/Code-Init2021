49b360463fc24b73af6f192d75e262d26fa9b3d8
Naveen <naveensd101@gmail.com>
2021-09-25 21:38:16 +0530

    add table component

---
+++ src/components/columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'First Name',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Last Name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Email',
+        accessor: 'email'
+    },
+    {
+        Header: 'Date of birth',
+        accessor: 'date_of_birth'
+    },
+]
