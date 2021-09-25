5f45f528923000069fa1cf84041b17b896a3ada0
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-09-25 21:52:11 +0400

    added console function and reset

--- src/formentry.js
+++ src/formentry.js
@@ -1,21 +1,41 @@
 const FormEntry = () => {
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const a=e.target.A.value;
+        const b=e.target.B.value;
+        const c=e.target.C.value;
+        const d=e.target.D.value;
+        const ee=e.target.E.value;
+        const f=e.target.F.value;
+        const g=e.target.G.value;
+        const h=e.target.H.value;
+        const p=e.target.P.value;
+        const q=e.target.Q.value;
+        const r=e.target.R.value;
+        const s=e.target.S.value;
+        const t=e.target.T.value;
+        const final={"A":a,"B":b,"C":c,"D":d,"E":ee,"F":f,"G":g,"H":h,"P":p,"Q":q,"R":r,"S":s,"T":t};
+        console.log(final);
+        e.target.reset();
+    }
+    
     return ( 
         <div className="entry">
             <h2>Enter core subjects/electives</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="two-col">
                     <div className="col1">
                         <label>A: </label>
                         <input 
                         type="text"
-                        
+                        name="A"
                         />
                     </div>
                     <div className="col2">
                         <label>B: </label>
                         <input 
                         type="text"
-                 
+                        name="B"
                         />
                     </div>
                     
@@ -26,14 +46,14 @@ const FormEntry = () => {
                         <label>C: </label>
                         <input 
                         type="text"
-               
+                        name="C"
                         />
                     </div>
                     <div className="col2">
                         <label>D: </label>
                         <input 
                         type="text"
-              
+                        name="D"
                         />
                     </div>
                     
@@ -44,14 +64,14 @@ const FormEntry = () => {
                         <label>E: </label>
                         <input 
                         type="text"
-                     
+                        name="E"
                         />
                     </div>
                     <div className="col2">
                         <label>F: </label>
                         <input 
                         type="text"
-                    
+                        name="F"
                         />
                     </div>
                     
@@ -61,14 +81,14 @@ const FormEntry = () => {
                         <label>G: </label>
                         <input 
                         type="text"
-                
+                        name="G"
                         />
                     </div>
                     <div className="col2">
                         <label>H: </label>
                         <input 
                         type="text"
-                    
+                        name="H"
                         />
                     </div>
                     
@@ -79,14 +99,14 @@ const FormEntry = () => {
                         <label>P: </label>
                         <input 
                         type="text"
-                 
+                        name="P"
                         />
                     </div>
                     <div className="col2">
                         <label>Q: </label>
                         <input 
                         type="text"
-                      
+                        name="Q"
                         />
                     </div>
                     
@@ -97,14 +117,14 @@ const FormEntry = () => {
                         <label>R: </label>
                         <input 
                         type="text"
-               
+                        name="R"
                         />
                     </div>
                     <div className="col2">
                         <label>S: </label>
                         <input 
                         type="text"
-               
+                        name="S"
                         />
                     </div>
                     
@@ -114,7 +134,7 @@ const FormEntry = () => {
                         <label>T: </label>
                         <input 
                         type="text"
-                     
+                        name="T"
                         />
                     </div>
                 </div>
